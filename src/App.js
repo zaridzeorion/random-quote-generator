11e5da27af16f3964a4e1f80de4e5d1a9b85430f
Zurab Zaridze <72822578+zaridzeorion@users.noreply.github.com>
2020-12-08 04:21:28 -0800

    commit random-quote-generator

--- src/App.js
+++ src/App.js
@@ -1,25 +1,101 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { quoteDatabase } from './quoteDatabase'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      quote:
+        quoteDatabase[Math.floor(Math.random() * quoteDatabase.length - 1)]
+          .quote,
+      author:
+        quoteDatabase[Math.floor(Math.random() * quoteDatabase.length - 1)]
+          .author
+    };
+    this.quoteAuthorChange = this.quoteAuthorChange.bind(this);
+  }
+
+  quoteAuthorChange() {
+    let randomQ =
+      quoteDatabase[Math.floor(Math.random() * quoteDatabase.length)];
+    this.setState({
+      quote: randomQ.quote,
+      author: randomQ.author
+    });
+  }
+
+  render() {
+    return (
+      <div id="wrapper">
+        <div id="quote-box">
+          <div className="quote-text">
+            <i className="fa fa-quote-left" />
+            <span className="text"> {this.state.quote} </span>
+            <i className="fa fa-quote-left" />
+          </div>
+          <div class="quote-author">
+            <span id="author">{this.state.author}</span>
+          </div>
+
+          <p className="Stoic-Quotes">Roman Emperor Quotes</p>
+
+          <div className="buttons">
+            <a
+              href="https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&text= You have power over your mind - not outside events. Realize this, and you will find strength ~ Marcus Aurelius"
+              className="button"
+              id="tweet-quote"
+              title="Tweet this quote!"
+              target="_blank"
+            >
+              <i className="fa fa-twitter"></i>
+            </a>
+            <a
+              href="https://facebook.com/intent/post?hashtags=quotes&related=freecodecamp&text= You have power over your mind - not outside events. Realize this, and you will find strength ~ Marcus Aurelius"
+              className="button"
+              id="facebook-quote"
+              title="Post this quote on facebook!"
+              target="_blank"
+            >
+              <i className="fa fa-facebook"></i>
+            </a>
+            <a
+              href="https://instagram.com/intent/post?hashtags=quotes&related=freecodecamp&text= You have power over your mind - not outside events. Realize this, and you will find strength ~ Marcus Aurelius"
+              className="button"
+              id="instagram-quote"
+              title="Post this quote on instagram story!"
+              target="_blank"
+            >
+              <i className="fa fa-instagram"></i>
+            </a>
+
+            <button
+              className="button"
+              id="new-quote"
+              title="Generate new quote!"
+              onClick={this.quoteAuthorChange}
+            >
+              New Quote
+            </button>
+          </div>
+        </div>
+        <div className="footer">
+          by{" "}
+          <a href="https://www.linkedin.com/in/zurab-zaridze/">Zurab Zaridze</a>
+          <br />
+          <br />
+          <a
+            style={{ color: "red" }}
+            href="https://www.behance.net/gallery/11729143/Dark-forest"
+            target="_blank"
+          >
+            Image Source
+          </a>
+        </div>
+      </div>
+    );
+  }
 }
+  
+
 
 export default App;
---
+++ src/quoteDatabase.js
@@ -0,0 +1,42 @@
+export const quoteDatabase = [
+    {
+      quote: "Waste no more time arguing what a good man should be. Be One.",
+      author: "Marcus Aurelius"
+    },
+    {
+      quote:
+        "It never ceases to amaze me: we all love ourselves more than other people, but care more about their opinion than our own.",
+      author: "Marcus Aurelius"
+    },
+    {
+      quote:
+        "If it is not right, do not do it, if it is not true, do not say it.",
+      author: "Marcus Aurelius"
+    },
+    {
+      quote:
+        "Dwell on the beauty of life. Watch the stars, and see yourself running with them.",
+      author: "Marcus Aurelius"
+    },
+    {
+      quote:
+        "You have power over your mind - not outside events. Realize this, and you will find strength",
+      author: "Marcus Aurelius"
+    },
+    {
+      quote:
+        "Everything we hear is an opinion, not a fact. Everything we see is a perspective, not the truth.",
+      author: "Marcus Aurelius"
+    },
+    {
+      quote:
+        "If you are distressed by anything external, the pain is not due to the thing itself, but to your estimate of it; and this you have the power to revoke at any moment.",
+      author: "Marcus Aurelius"
+    },
+    {
+      quote:
+        "When you arise in the morning think of what a privilege it is to be alive, to think, to enjoy, to love ...",
+      author: "Marcus Aurelius"
+    }
+  ];
+  
